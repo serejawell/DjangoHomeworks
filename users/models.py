95675762b5386618054281d9c234daf051790902
serejawell <sev2316@yandex.ru>
2024-10-12 02:57:30 +0300

    22.2 Homework

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True,
+            'blank': True}
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True,verbose_name='почта')
+
+    phone = models.CharField(max_length=35,verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар')
+    country = models.CharField(max_length=40,verbose_name='страна', **NULLABLE)
+    token = models.CharField(max_length=100,verbose_name='токен',**NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
---
+++ users/views.py
@@ -0,0 +1,90 @@
+import random
+import string
+
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.hashers import make_password
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView
+import secrets
+from users.forms import UserRegisterForm, UserProfileForm, PasswordResetForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}'
+        send_mail(
+            subject='Welcome to my new site!',
+            message=f'Link for confirm email: {url}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+        return super().form_valid(form)
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+def generate_random_password(length=8):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
+
+
+def user_reset_password(request):
+    form = PasswordResetForm()
+
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            try:
+                user = User.objects.get(email=email)
+                new_password = generate_random_password()
+                user.password = make_password(new_password)
+                user.save()
+                send_mail(
+                    subject='Восстановление пароля',
+                    message=f'Ваш новый пароль: {new_password}',
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[email],
+                    fail_silently=False,
+                )
+                print(f'New password: {new_password}')  # Для проверки нового пароля
+                messages.success(request, 'Новый пароль отправлен на вашу почту.')
+
+            except User.DoesNotExist:
+                messages.error(request, 'Пользователь с таким email не найден.')
+            except Exception as e:
+                messages.error(request, f'Произошла ошибка: {str(e)}')
+
+    # Возврат рендеринга формы вне блока if
+    return render(request, 'users/password_reset.html', {'form': form})
